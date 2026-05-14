b17c38dfa866a27a7223ed94f009718aafb149ef
Jeremi Maciejewski <jeremaciejewski@gmail.com>
2026-05-14 19:38:36 +0200

    Implement drawing of the cells and scrolling of the camera using arrow keys; make some improvements to class ConwayGraphics; (bit more) gracefully handle YAML loader errors

--- conway_graphics.py
+++ conway_graphics.py
@@ -9,23 +9,31 @@ class ConwayGraphics:
         self.surfaces = surfaces
         self.consts = consts
 
-    # ConwayGraphics[key] is synonymous to ConwayGraphics.consts[key]
+    # ConwayGraphics[key] is synonymous to ConwayGraphics.consts.get(key, None)
+    # Note that this does not allow for assigning/modifying - use ConwayGraphics.setsurf() for that
     def __getitem__(self, subscript):
         try:
-            return self.consts[subscript]
+            return self.consts.get(subscript, None)
         except (TypeError, LookupError, KeyError) as e:
             raise e
 
-    # ConwayGraphics@key is synonymous to ConwayGraphics.surfaces[key]
-    # Note that this operator has lower precedence, though
+    # ConwayGraphics@key is synonymous to ConwayGraphics.surfaces.get(key, None)
+    # Note that this operator has lower precedence and this does not allow for assigning (use
+    # ConwayGraphics.setconst() for that)
     def __matmul__(self, subscript):
         try:
-            return self.surfaces[subscript]
+            return self.surfaces.get(subscript, None)
         except (TypeError, LookupError, KeyError) as e:
             raise e
 
+    def setsurf(self, subscript, value):
+        self.surfaces[subscript] = value
 
-def init():
+    def setconst(self, subscript, value):
+        self.consts[subscript] = value
+
+
+def init(width, height):
     pygame.init()
 
     info = pygame.display.Info()
@@ -36,6 +44,10 @@ def init():
     # The game window will be a square taking up 90% of shorter dimension of the screen,
     # rounded down to a round number of cells
     win_size = int(min(scr_res) * 0.9 // cs * cs)
+    cells_visible = win_size // cs
+    # Note that scroll is written down as movement of the map, rather than camera
+    # (i.e. it might seem that the values are inverted)
+    initial_scroll = ((width/2 - cells_visible/2)*cs*-1, (height/2 - cells_visible/2)*cs*-1)
 
     pygame.display.set_caption("Conway's Game of Life")
     window = pygame.display.set_mode((win_size, win_size))
@@ -54,6 +66,21 @@ def init():
         pygame.draw.line(bg, mesh_color, (0, pos-1), (win_size+cs*2, pos-1), 2)
 
     cg = ConwayGraphics(win_size, {"window":window, "background":bg},
-                        {"resolution":scr_res, "scroll":(0,0), "cell_size":cs})
+                        {"resolution":scr_res, "scroll":initial_scroll, "cell_size":cs})
 
     return cg
+
+
+def draw_map(graphics_data, map):
+    gd = graphics_data
+    if not gd@"cells":
+        gd.setsurf("cells", pygame.Surface((gd@"background").get_size()).convert_alpha())
+
+    (gd@"cells").fill((0,0,0,0))
+
+    for x in range(len(map)):
+        for y in range(len(map[x])):
+            if map[x][y] == 1:
+                cellpos = (round(x*gd["cell_size"] + gd["cell_size"]/2 + gd["scroll"][0]),
+                            round(y*gd["cell_size"] + gd["cell_size"]/2 + gd["scroll"][1]))
+                pygame.draw.circle(gd@"cells", (255,0,0,255), cellpos, 5)
--- py-conway.py
+++ py-conway.py
@@ -1,4 +1,5 @@
 import os
+import time
 import argparse
 import pathlib
 import yaml
@@ -6,6 +7,9 @@ os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1" # Suppress pygame welcome message
 import pygame
 
 import conway_graphics as graphics
+
+# Lambda that adds 2 vectors positionally
+vectadd = lambda x, y: [x[i] + y[i] for i in range(min(len(x), len(y)))]
 
 def create_parser():
     parser = argparse.ArgumentParser(
@@ -22,14 +26,24 @@ def create_parser():
         help="Opcjonalnie, ścieżka do wynikowego pliku GIF dokumentującego przebieg symulacji (liczba wynikowych iteracji jest ograniczana poprzez plik konfiguracyjny).",
     )
 
-    # TODO: Check data validity
-
     return parser
 
 
 def load_config(config_file):
-    f = open(config_file, 'r')
-    conf = yaml.safe_load(f)
+    try:
+        f = open(config_file, 'r')
+    except OSError:
+        raise ValueError(f"The config file \"{config_file}\" could not be opened. Please verify whether the path is valid and whether you have permissions necessary to open it.") from None
+
+    try:
+        conf = yaml.safe_load(f)
+    except yaml.YAMLError as e:
+        suffix = ""
+        if hasattr(e, "problem"):
+            suffix += f"\nError description: \"{e.problem}\""
+        if hasattr(e, "problem_mark"):
+            suffix += f"\nYAML line {e.problem_mark.line+1}, column {e.problem_mark.column+1}"
+        raise ValueError(f"Error while parsing YAML: the data contained within configuration file \"{config_file}\" does not appear to be a valid YAML!{suffix}") from None
 
     return conf
 
@@ -59,14 +73,11 @@ def main(args=None):
     conf = load_config(args.config)
     map = make_map(conf["game"]["width"], conf["game"]["height"], conf["cells"])
 
-    #for y in range(conf["game"]["height"]):
-    #    [print(str(map[x][y])+' ', end='') for x in range(conf["game"]["width"])]
-    #    print()
-
     # Create ConwayGraphics object for current device
-    cg = graphics.init()
+    cg = graphics.init(conf["game"]["width"], conf["game"]["height"])
 
     running = True
+    arrowsdown = [0,0,0,0] # left-top-right-bottom
     while running: # Main loop
 
         # Handle user events
@@ -74,9 +85,52 @@ def main(args=None):
             if event.type == pygame.QUIT:
                 running = False
 
+            elif event.type == pygame.KEYDOWN:
+                match event.key:
+                    case pygame.K_ESCAPE:
+                        running = False
+                    case pygame.K_LEFT:
+                        arrowsdown[0] = 1
+                    case pygame.K_UP:
+                        arrowsdown[1] = 1
+                    case pygame.K_RIGHT:
+                        arrowsdown[2] = 1
+                    case pygame.K_DOWN:
+                        arrowsdown[3] = 1
 
-        (cg@"window").blit(cg@"background", (-cg["cell_size"],-cg["cell_size"]))
+            elif event.type == pygame.KEYUP:
+                match event.key:
+                    case pygame.K_LEFT:
+                        arrowsdown[0] = 0
+                    case pygame.K_UP:
+                        arrowsdown[1] = 0
+                    case pygame.K_RIGHT:
+                        arrowsdown[2] = 0
+                    case pygame.K_DOWN:
+                        arrowsdown[3] = 0
+
+        # Update scroll (5px/tick)
+        # Note that scroll is written down as movement of the map, rather than camera
+        # (i.e. it might seem that the values are inverted)
+        scrollchange = [0,0]
+        if arrowsdown[0]:
+            scrollchange[0] += 10
+        if arrowsdown[1]:
+            scrollchange[1] += 10
+        if arrowsdown[2]:
+            scrollchange[0] -= 10
+        if arrowsdown[3]:
+            scrollchange[1] -= 10
+
+        cg.setconst("scroll", vectadd(cg["scroll"], scrollchange) )
+
+        (cg@"window").blit(cg@"background", (-cg["cell_size"] + cg["scroll"][0]%cg["cell_size"],
+                                                -cg["cell_size"] + cg["scroll"][1]%cg["cell_size"]))
+        graphics.draw_map(cg, map)
+        (cg@"window").blit(cg@"cells", (-cg["cell_size"], -cg["cell_size"]))
         pygame.display.flip()
+
+        time.sleep(0.05)
 
 
 if __name__ == "__main__":
